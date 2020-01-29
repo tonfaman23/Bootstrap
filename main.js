e01874e1b7dfbb2c195d044b230004d591c1b78d
Will Baker <1will.baker@gmail.com>
2020-01-28 19:16:38 -0600

    array of ducks added

--- main.js
+++ main.js
@@ -1,1 +1,91 @@
-console.log("Learning Bootstrap")
+const ducks = [
+    {
+        isRubber: true,
+        color: 'blue',
+        gender: 'female',
+        isMigrator: false,
+        socialStatus: 'ready to mingle',
+        diet: 'none',
+        age: 55,
+        featherNum: 0,
+        name: 'Rubber Ducky',
+        imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/31q-v-h3cQL._SX355_.jpg'
+    },
+    {
+        isRubber: true,
+        color: 'yellow',
+        gender: 'female',
+        isMigrator: false,
+        socialStatus: 'ready to mingle',
+        diet: 'none',
+        age: 67,
+        featherNum: 0,
+        name: 'Nora',
+        imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/8166xCVDGnL._SY355_.jpg'
+    },
+    {
+        isRubber: false,
+        color: 'green',
+        gender: 'male',
+        isMigrator: true,
+        socialStatus: 'ready to mingle',
+        diet: 'vegan',
+        age: 33,
+        name: 'Norbert',
+        imgUrl: 'https://i.pinimg.com/originals/a5/f5/57/a5f557ba7002237e2e6ac0fa11a6d019.jpg'
+    },
+    {
+        isRubber: false,
+        color: 'green',
+        gender: 'male',
+        isMigrator: true,
+        socialStatus: 'ready to mingle',
+        diet: 'vegan',
+        age: 27,
+        name: 'Alexander',
+        imgUrl: 'https://live.staticflickr.com/3115/3179800771_e0f22306b9_b.jpg'
+    },
+    {
+        isRubber: false,
+        color: 'brown',
+        gender: 'female',
+        isMigrator: true,
+        socialStatus: 'looking for partner',
+        diet: 'vegan',
+        age: 32,
+        name: 'Yvette',
+        imgUrl: 'https://www.allaboutbirds.org/guide/assets/photo/60021801-480px.jpg'
+    },
+    {
+        isRubber: true,
+        color: 'orange',
+        gender: 'male',
+        isMigrator: false,
+        socialStatus: 'ready to mingle',
+        diet: 'none',
+        name: 'Gudao',
+        imgUrl: 'https://www.allaboutbirds.org/guide/assets/photo/60021801-480px.jpg'
+    },
+    {
+        isRubber: true,
+        color: 'blue',
+        gender: 'male',
+        isMigrator: false,
+        socialStatus: 'loner',
+        diet: 'instant food',
+        name: 'Deadbeat',
+        imgUrl: 'https://cdn.shopify.com/s/files/1/0604/4801/products/Blue_3.jpeg?v=1514845837'
+    },
+    {
+        isRubber: true,
+        color: 'yellow',
+        gender: 'male',
+        isMigrator: false,
+        socialStatus: 'party animal',
+        diet: 'luxury meals',
+        name: 'Prince',
+        imgUrl: 'https://cdn11.bigcommerce.com/s-jnapaiw/images/stencil/1280x1280/products/2945/4049/Sunny_duck__52036.1400093435.jpg?c=2'
+    }
+]
+
+console.log(ducks);
